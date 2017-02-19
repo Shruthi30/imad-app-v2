80f3aadb90ec003a1c15f620ca40b33201a48deb
Shruthi30 <shruthi.sgj@gmail.com>
2017-02-19 19:14:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,2 @@
 console.log('Loaded!');
 
-//change text
-var element = document.getElementById('main-text'
-        );
-
-element.innerHTML = 'New Value';
-//move the image
-
-var img = document.getElementById('madi');
-var marginRight = 0;
-function moveLeft () {
-    marginRight = marginRight + 10;
-    img.style.marginRight = marginRight + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveLeft, 100);
-};
